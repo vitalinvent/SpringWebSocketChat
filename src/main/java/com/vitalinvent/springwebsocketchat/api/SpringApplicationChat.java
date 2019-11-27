a619c8c60980d64a47924c6056b4157fab037941
vitalinvent <vitalinvent@gmail.com>
2019-11-27 12:02:50 +0300

    fix spring boot app annot

--- src/main/java/com/vitalinvent/springwebsocketchat/api/SpringApplicationChat.java
+++ src/main/java/com/vitalinvent/springwebsocketchat/api/SpringApplicationChat.java
@@ -1,7 +1,9 @@
 package com.vitalinvent.springwebsocketchat.api;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class SpringApplicationChat {
 
     public static void main(String[] args){
--- src/main/java/com/vitalinvent/springwebsocketchat/config/Configuration.java
+++ src/main/java/com/vitalinvent/springwebsocketchat/config/Configuration.java
@@ -11,7 +11,7 @@ public class Configuration implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat").withSockJS();
+        registry.addEndpoint("/SpringApplicationChat").withSockJS();
     }
 
     @Override
